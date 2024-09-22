9665c1850d658686342646b41aadbc0d1fc96964
Francisco Cedermaz <francedermaz@gmail.com>
2024-09-22 17:01:26 -0300

    feat: update and use movieresponse

--- app/src/main/java/com/example/filmhub/api/ApiService.java
+++ app/src/main/java/com/example/filmhub/api/ApiService.java
@@ -1,6 +1,7 @@
 package com.example.filmhub.api;
 
 import com.example.filmhub.models.Movie;
+import com.example.filmhub.models.MovieResponse;
 
 import java.util.List;
 
@@ -8,6 +9,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiService {
-    @GET("movie/popular?api_key=")
-    Call<List<Movie>> getPopularMovies();
+    @GET("movie/popular?api_key=50496d349af7ede42dc06e6fb73c7cce")
+    Call<MovieResponse> getPopularMovies();
 }
