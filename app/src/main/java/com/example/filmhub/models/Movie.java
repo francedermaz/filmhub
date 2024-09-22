20648be33185d5b32e3d2359c0358f2eaf5dbd76
Francisco Cedermaz <francedermaz@gmail.com>
2024-09-22 15:56:30 -0300

    feat: add model movie

---
+++ app/src/main/java/com/example/filmhub/models/Movie.java
@@ -0,0 +1,50 @@
+package com.example.filmhub.models;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String overview;
+    private String release_date;
+    private double vote_average;
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getReleaseDate() {
+        return release_date;
+    }
+
+    public void setReleaseDate(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public double getVoteAverage() {
+        return vote_average;
+    }
+
+    public void setVoteAverage(double vote_average) {
+        this.vote_average = vote_average;
+    }
+}
